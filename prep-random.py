7ab5ca3349cf94be8824f1c9701af970d5dfae19
Matthew Rocklin <mrocklin@gmail.com>
2015-07-20 18:41:18 -0700

    clean slate

--- prep-random.py
+++
@@ -1,9 +0,0 @@
-import h5py
-import numpy as np
-import h5py
-import os
-
-f = h5py.File(os.path.join('data', 'tmp.hdf5'))
-dset = f.create_dataset('/x', shape=(1000000000,), dtype='f4')
-for i in range(0, 1000000000, 1000000):
-    dset[i: i + 1000000] = np.random.exponential(size=1000000)
