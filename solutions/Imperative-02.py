30df12d1e84a67e6bcf7ca8d74e5bc1aa01b01ce
Matthew Rocklin <mrocklin@gmail.com>
2015-07-23 14:17:59 -0700

    update imperative random exercise

--- solutions/Imperative-02.py
+++ solutions/Imperative-02.py
@@ -1,17 +1,7 @@
-@do
-def point_chunk():
-    """Generates a random x, y point, returns 1 if in circle, else returns 0."""
-    x = random()
-    y = random()
-    if x*x + y*y <= 1:
-        return 1
-    else:
-        return 0
+from dask import do
 
 def parallel_estimate_pi(nsamples):
-    points = []
-    for i in range(nsamples):
-        points.append(point_chunk())
-    total = do(sum)(points)
-    result = 4.*total/nsamples
-    return result.compute(get=get)
+    points = [do(is_inside_circle)() for i in range(nsamples)]
+    return 4. * do(sum)(points) / nsamples
+
+print(parallel_estimate_pi(10000).compute())
--- solutions/Imperative-03.py
+++ solutions/Imperative-03.py
@@ -1,19 +1,13 @@
-@do
-def total_chunk(nsamples):
-    """Generates `nsamples` random x, y points, returns number of 
-    points that were in the circle.
-    """
-    total = 0
-    for i in range(nsamples):
-        x = random()
-        y = random()
-        if x*x + y*y <= 1:
-            total += 1
-    return total
+from dask import do
 
-def parallel_estimate_pi(nsamples, npartitions):
-    totals = []
-    for n in partition(nsamples, npartitions):
-        totals.append(total_chunk(n))
-    result = 4.*do(sum)(totals)/nsamples
-    return result.compute(get=get)
+def how_many_inside_circle(k):
+    return sum(is_inside_circle() for i in range(k))
+
+def parallel_estimate_pi(nsamples, k):
+    points = [do(how_many_inside_circle)(k)
+              for i in range(int(nsamples / k))]
+    if nsamples % k != 0:   # doesn't divide cleanly
+        points.append(do(how_many_inside_circle(nsamples % k)))
+    return 4. * do(sum)(points) / nsamples
+
+print(parallel_estimate_pi(10000000, 100000).compute())
