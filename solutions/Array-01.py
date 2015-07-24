23a5f008271493e8cac6eb82c27e375f5da25212
Matthew Rocklin <mrocklin@gmail.com>
2015-07-23 21:31:25 -0700

    simplify array exercise

--- solutions/Array-01.py
+++ solutions/Array-01.py
@@ -1,7 +1,10 @@
 sums = []
+lengths = []
 for i in range(0, 1000000000, 1000000):
     chunk = dset[i: i + 1000000]  # pull out numpy array
-    sums.append((chunk > 1).sum())
+    sums.append(chunk.sum())
+    lengths.append(len(chunk))
 
 total = sum(sums)
-print(total)
+length = sum(lengths)
+print(total / length)
