7d4cd4ccdb1254bb481568a5904486b1e22387d2
Albert DeFusco <albert.defusco@me.com>
2017-02-01 00:12:05 +0100

    replace do with delayed

--- solutions/Imperative-03.py
+++ solutions/Imperative-03.py
@@ -1,10 +1,10 @@
-from dask import do
+from dask import delayed
 
 def how_many_inside_circle(k):
     return sum(is_inside_circle() for i in range(k))
 
 def parallel_estimate_pi(nsamples, k):
-    points = [do(how_many_inside_circle)(k)
+    points = [delayed(how_many_inside_circle)(k)
               for i in range(int(nsamples / k))]
     if nsamples % k != 0:   # doesn't divide cleanly
         points.append(do(how_many_inside_circle(nsamples % k)))
