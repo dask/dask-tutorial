1c9b2d28241e984f028c1d573ae1b2ac5f9104ef
Martin Durant <martin.durant@utoronto.ca>
2017-02-08 19:15:07 -0500

    Add solution to exercise in 02

---
+++ solutions/Foundations-03.py
@@ -0,0 +1,23 @@
+from dask.multiprocessing import get
+
+# verbose version
+delayed_read_csv = delayed(pd.read_csv)
+a = delayed_read_csv(filenames[0])
+b = delayed_read_csv(filenames[1])
+c = delayed_read_csv(filenames[2])
+
+delayed_len = delayed(len)
+na = delayed_len(a)
+nb = delayed_len(b)
+nc = delayed_len(c)
+
+delayed_sum = delayed(sum)
+
+total = delayed_sum([na, nb, nc])
+%time print(total.compute(get=get))
+
+# concise version
+csvs = [delayed(pd.read_csv)(fn) for fn in filenames]
+lens = [delayed(len)(csv) for csv in csvs]
+total = delayed(sum)(lens)
+%time print(total.compute(get=get))
