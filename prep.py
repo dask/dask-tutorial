d9018fb459f0d3cfc0698510e9c0875970c31d29
Andrew Loucky <andrew.loucky@gmail.com>
2018-07-14 11:40:26 -0400

    Remove 2 data/ existence checks, add small-weather check
    Since data/ exists at clone, we do not have to check that it exists, let
    alone twice. However, adding a check that there are
    small-weather/*.hdf5 files as a "just in case" check.

--- prep.py
+++ prep.py
@@ -7,19 +7,12 @@ import urllib.request
 import zipfile
 
 here = os.path.dirname(__file__)
-
 data_dir = os.path.join(here, 'data')
-if not os.path.exists(data_dir):
-    os.mkdir(data_fir)
-
 
 def flights():
     flights_raw = os.path.join(data_dir, 'nycflights.tar.gz')
     flightdir = os.path.join(data_dir, 'nycflights')
     jsondir = os.path.join(data_dir, 'flightjson')
-
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
 
     if not os.path.exists(flights_raw):
         print("- Downloading NYC Flights dataset... ", end='', flush=True)
@@ -98,6 +91,10 @@ def accounts_json(num_files, n, k):
 
 def create_weather(growth=32):
     filenames = sorted(glob(os.path.join('data', 'weather-small', '*.hdf5')))
+
+    if not filenames:
+            ws_dir = os.path.join('data', 'weather-small')
+            raise ValueError("Did not find any hdf5 files in {}".format(ws_dir))
 
     if not os.path.exists(os.path.join('data', 'weather-big')):
         os.mkdir(os.path.join('data', 'weather-big'))
