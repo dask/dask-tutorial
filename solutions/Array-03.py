210fa0e46e2dabf28983532613f66f09bfb55dd8
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-12-06 16:32:32 -0600

    more

--- solutions/Array-03.py
+++ solutions/Array-03.py
@@ -1,15 +1,15 @@
 import h5py
 from glob import glob
 import os
+import dask.array as da
 
 filenames = sorted(glob(os.path.join('data', 'weather-big', '*.hdf5')))
 dsets = [h5py.File(filename, mode='r')['/t2m'] for filename in filenames]
 
-import dask.array as da
 arrays = [da.from_array(dset, chunks=(500, 500)) for dset in dsets]
 
 x = da.stack(arrays, axis=0)
 
 result = x[:, ::2, ::2]
 
-da.to_hdf5(os.path.join('data', 'myfile.hdf5'), '/output', result)
+da.to_zarr(result, os.path.join('data', 'myfile.zarr'), overwrite=True)
