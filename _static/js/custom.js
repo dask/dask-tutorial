f78bbda028e306a5ec293358586d15e4e89c5f1d
jacobtomlinson <jacobtomlinson@users.noreply.github.com>
2021-10-14 16:00:11 +0000

    Deploying to gh-pages from  @ f4d0867f2373ff9e8b08e46e9c6eb9f89c101f6b 🚀

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,5 +1,8 @@
 if (location.protocol == "http:") {
-  if (!window.location.href.includes("localhost")) {
+  if (
+    !window.location.href.includes("localhost") &&
+    !window.location.href.includes("127.0.0.1")
+  ) {
     location.href =
       "https:" +
       window.location.href.substring(window.location.protocol.length);
